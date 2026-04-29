dbb9e6c5aac3f95d953a06d8ddfa6f51720cd0b2
manikandansivagnanam <manikandan.sr0110@gmail.com>
2026-04-29 01:59:12 +0000

    Deploying to gh-pages from @ manikandansivagnanam/manikandansivagnanam.github.io@a8cc9c2c7e5c51799b97b7ed4106e544bd6520c7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -395,7 +395,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "teachings-data-science-fundamentals",
+          section: "News",},{id: "projects-",
+          title: '',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
           section: "Teachings",handler: () => {
