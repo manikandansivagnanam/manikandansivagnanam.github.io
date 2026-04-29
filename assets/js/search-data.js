9ea0ae3c7481a211113b0e731d02a4b2dec6a40b
manikandansivagnanam <manikandan.sr0110@gmail.com>
2026-04-29 02:03:54 +0000

    Deploying to gh-pages from @ manikandansivagnanam/manikandansivagnanam.github.io@88e081ddfeb080acc0e25b807ea0643a9659d87f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
+  },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of my research and computational projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
