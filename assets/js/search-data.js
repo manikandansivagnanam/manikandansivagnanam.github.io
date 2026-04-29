ca683b5ba40b3dfa4f674e8cef1fb99211935176
manikandansivagnanam <manikandan.sr0110@gmail.com>
2026-04-29 00:16:57 +0000

    Deploying to gh-pages from @ manikandansivagnanam/manikandansivagnanam.github.io@6fa674ecd22e0617c66464bd86262076a739c1bf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-causal-mediation-of-gut-microbiota-plasma-metabolites-amp-adhd",
+          title: 'Causal Mediation of Gut Microbiota, Plasma Metabolites &amp;amp; ADHD',
+          description: "Multi-omics integration using Mendelian Randomization.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
