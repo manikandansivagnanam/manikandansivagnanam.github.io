937ffd32f41d0eef360c541def6393cc3170cf74
manikandansivagnanam <manikandan.sr0110@gmail.com>
2026-04-29 01:40:29 +0000

    Deploying to gh-pages from @ manikandansivagnanam/manikandansivagnanam.github.io@1c8fa2e0af2f5bd9fa8939a39a2148b4944ef19b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -407,11 +407,6 @@ ninja.data = [{
           description: "Multi-omics integration using Mendelian Randomization.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
